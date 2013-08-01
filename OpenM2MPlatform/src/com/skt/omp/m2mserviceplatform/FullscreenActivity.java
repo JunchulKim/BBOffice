e22642ac3b4d3a409882732bd52e4e59fe0989ac
jspark <jspark@tecace.com>
2013-08-01 20:55:34 +0900

    test

--- OpenM2MPlatform/src/com/skt/omp/m2mserviceplatform/FullscreenActivity.java
+++ OpenM2MPlatform/src/com/skt/omp/m2mserviceplatform/FullscreenActivity.java
@@ -199,6 +199,8 @@ public class FullscreenActivity extends Activity {
           HttpEntity entity = response.getEntity();
           */
         	
+        	
+        	
             
         } catch (Exception e) {
             e.printStackTrace();
